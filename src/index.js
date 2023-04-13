c2fbdca11cc233213f819cea4c0e60698e869482
Adrian Karp <Adrian.Karp@ibm.com>
2023-04-13 16:08:22 +0300

    fixed port bug

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use('/', sampleRoutes);
 
-const port = 3000;
+const port = 3001;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
